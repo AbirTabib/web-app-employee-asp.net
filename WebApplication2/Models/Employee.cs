c67d947656da1a0172a70563c650fdc20763e1f9
AbirTabib <abir.tabib.497@gmail.com>
2023-01-14 21:27:15 +0100

    first commit

---
+++ WebApplication2/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required,StringLength(100,ErrorMessage ="Taille max 10 characters")]
+        public string Name { get; set; }
+        [Required]
+        public string Departement { get; set; }
+        [Range(200,5000)]
+        public int Salary { get; set; }
+        public bool Equals(Employee other)
+        {
+            return this.Id == other.Id;
+        }
+    }
+}
